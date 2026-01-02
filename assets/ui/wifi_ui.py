0daa402a4121e470d380f9ff770d82251fbb95e2
Lluciocc <llucio.cc00@gmail.com>
2026-01-03 00:07:36 +0100

    less inconventianal comments

--- assets/ui/wifi_ui.py
+++ assets/ui/wifi_ui.py
@@ -24,14 +24,12 @@ class PasswordDialog(Gtk.Dialog):
         box.set_margin_end(20)
         box.set_margin_top(20)
         box.set_margin_bottom(20)
-        
-        # Network info
+
         info_label = Gtk.Label()
         info_label.set_markup(f"<b>Network:</b> {ssid}\n<b>Security:</b> {security}")
         info_label.set_xalign(0)
         box.pack_start(info_label, False, False, 0)
-        
-        # Password entry
+
         pwd_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
         pwd_label = Gtk.Label(label="Password:")
         pwd_label.set_width_chars(10)
@@ -42,8 +40,7 @@ class PasswordDialog(Gtk.Dialog):
         self.password_entry.set_input_purpose(Gtk.InputPurpose.PASSWORD)
         self.password_entry.set_activates_default(True)
         pwd_box.pack_start(self.password_entry, True, True, 0)
-        
-        # Show password with revealer
+
         self.show_pwd_btn = Gtk.ToggleButton()
         eye_icon = Gtk.Image.new_from_icon_name("view-reveal-symbolic", Gtk.IconSize.BUTTON)
         self.show_pwd_btn.set_image(eye_icon)
@@ -52,8 +49,7 @@ class PasswordDialog(Gtk.Dialog):
         pwd_box.pack_start(self.show_pwd_btn, False, False, 0)
         
         box.pack_start(pwd_box, False, False, 0)
-        
-        # Revealer for password strength indicator
+
         self.revealer = Gtk.Revealer()
         self.revealer.set_transition_type(Gtk.RevealerTransitionType.SLIDE_DOWN)
         
@@ -107,14 +103,12 @@ class HiddenNetworkDialog(Gtk.Dialog):
         box.set_margin_end(20)
         box.set_margin_top(20)
         box.set_margin_bottom(20)
-        
-        # Info label
+
         info = Gtk.Label()
         info.set_markup("<b>Enter hidden network details:</b>")
         info.set_xalign(0)
         box.pack_start(info, False, False, 0)
-        
-        # SSID entry
+
         ssid_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
         ssid_label = Gtk.Label(label="SSID:")
         ssid_label.set_width_chars(10)
@@ -124,8 +118,7 @@ class HiddenNetworkDialog(Gtk.Dialog):
         self.ssid_entry.set_placeholder_text("Network name")
         ssid_box.pack_start(self.ssid_entry, True, True, 0)
         box.pack_start(ssid_box, False, False, 0)
-        
-        # Password entry
+
         pwd_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
         pwd_label = Gtk.Label(label="Password:")
         pwd_label.set_width_chars(10)
@@ -136,8 +129,7 @@ class HiddenNetworkDialog(Gtk.Dialog):
         self.password_entry.set_input_purpose(Gtk.InputPurpose.PASSWORD)
         self.password_entry.set_placeholder_text("Leave empty for open network")
         pwd_box.pack_start(self.password_entry, True, True, 0)
-        
-        # Show password toggle
+
         self.show_pwd_btn = Gtk.ToggleButton()
         eye_icon = Gtk.Image.new_from_icon_name("view-reveal-symbolic", Gtk.IconSize.BUTTON)
         self.show_pwd_btn.set_image(eye_icon)
@@ -171,8 +163,7 @@ class LogViewerDialog(Gtk.Dialog):
         box.set_margin_end(12)
         box.set_margin_top(12)
         box.set_margin_bottom(12)
-        
-        # Text view for logs
+
         scroll = Gtk.ScrolledWindow()
         scroll.set_vexpand(True)
         
@@ -200,14 +191,12 @@ class LogViewerDialog(Gtk.Dialog):
                     buffer.set_text("No connection history yet.")
         else:
             buffer.set_text("No connection history yet.")
-        
-        # Scroll to end
+
         end_iter = buffer.get_end_iter()
         self.text_view.scroll_to_iter(end_iter, 0.0, False, 0.0, 0.0)
     
     def on_response(self, dialog, response):
         if response == Gtk.ResponseType.APPLY:
-            # Clear history
             if HISTORY_FILE.exists():
                 HISTORY_FILE.unlink()
             buffer = self.text_view.get_buffer()
